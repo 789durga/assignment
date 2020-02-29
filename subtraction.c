fc3e0e7a7165fd7cabc228a2337a0d5202a09c3c
789durga <61613358+789durga@users.noreply.github.com>
2020-02-29 15:03:39 +0530

    Update subtraction.c

--- subtraction.c
+++ subtraction.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main() {    
 
-    int number1, number2, diff;
+    int number1, number2, difference;
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
 
     // calculating sum
-    diff = number1 + number2;      
+    difference = number1 + number2;  
+//     adding changes
     
-    printf("%d + %d = %d", number1, number2, diff);
+    printf("%d + %d = %d", number1, number2, difference);
     return 0;
 }
